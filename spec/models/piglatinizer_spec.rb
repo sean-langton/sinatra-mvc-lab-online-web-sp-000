c28bf3d2ff0fb06195d4a96fc50ccfedad6c6c81
Sean Langton <slangton1@gmail.com>
2019-01-30 20:45:37 +0000

    Automatically backed up by Learn

--- spec/models/piglatinizer_spec.rb
+++ spec/models/piglatinizer_spec.rb
@@ -8,7 +8,7 @@ describe 'class PigLatinizer' do
 
   it 'piglatinizes an individual word' do
     expect(words.piglatinize("pork")).to eq("orkpay")
-#    expect(words.piglatinize("I")).to eq("Iway")
+    expect(words.piglatinize("I")).to eq("Iway")
     expect(words.piglatinize("hello")).to eq("ellohay")
 #    expect(words.piglatinize("please")).to eq("easeplay")
 #    expect(words.piglatinize("tomorrow")).to eq("omorrowtay")
