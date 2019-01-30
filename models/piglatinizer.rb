9e0acda54ddf0d61c8ee69b83986216739927baa
Sean Langton <slangton1@gmail.com>
2019-01-30 20:33:35 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,21 +4,15 @@ class PigLatinizer
   end
 
   def piglatinize(phrase)
-#    word_array = phrase.split
-#    word_array.collect do |word|
-#      until word.start_with?("a","e","i","o","u")
-#        word << word[0]
-#        word[0] = ""
-#      end
-#        word << "ay"
-#    end
-#    word_array.join(" ")
-    until phrase.start_with?("a","e","i","o","u")
-        phrase << phrase[0]
-        phrase = phrase[1..-1]
+    word_array = phrase.split
+    word_array.collect do |word|
+      until word.start_with?("a","e","i","o","u")
+        word << word[0]
+        word[0] = ""
       end
-        phrase << "ay"
-        return phrase
+        word << "ay"
+    end
+    word_array.join(" ")
   end
 
 end
