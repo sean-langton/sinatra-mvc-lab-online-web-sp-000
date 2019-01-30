d147694946a0fdb66c3df812b2d3c5c215143381
Sean Langton <slangton1@gmail.com>
2019-01-30 20:52:29 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,11 +4,10 @@ class PigLatinizer
   end
 
   def piglatinize(phrase)
-    vowels = ["a","e","i","o","u","A","E","I","O","U"]
     word_array = phrase.split
     word_array.collect do |word|
-      word << "w" if word.start_with?(vowels)
-      until word.start_with?(vowels)
+      word << "w" if word.start_with?("a","e","i","o","u","A","E","I","O","U")
+      until word.start_with?("a","e","i","o","u","A","E","I","O","U")
         word << word[0]
         word[0] = ""
       end
